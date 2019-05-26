8d5494004d91e9f6740ef416311383341a2da94d
Geqiao <171250029@smail.nju.edu.cn>
2019-05-26 23:57:15 +0800

    增加info页面course展示

--- src/views/user/course/info.vue
+++ src/views/user/course/info.vue
@@ -2,6 +2,34 @@
   <div>
     <div class="course-info">
       <el-container>
+        <el-aside>
+          <div class="image">
+            <img :src="course.posterUrl">
+          </div>
+        </el-aside>
+        <el-container direction="vertical">
+          <el-main>
+            <div class="course-title">{{course.name}}</div>
+            <div class="course-des">
+              <div>{{course.teacher}}</div>
+              <div>{{course.description}}</div>
+            </div>
+          </el-main>
+          <el-foot>
+            <div class="test">
+              <div style="text-align: left; font-size: 12px">评分</div>
+              <div>
+                <el-rate
+                  v-model="course.score"
+                  disabled
+                  show-score
+                  text-color="#ff9900"
+                  score-template="{value}"
+                />
+              </div>
+            </div>
+          </el-foot>
+        </el-container>
       </el-container>
       <el-tabs tab-position="left" style="height: 100%;" :stretch="true" @tab-click="handleClick">
         <el-tab-pane v-for="(chapter, index) in course.chapters" :key="index" :label="chapter.name">
@@ -25,43 +53,6 @@ export default {
   components: {
     // 'course-card': CourseCard,
     'note-card': NoteCard
-  },
-  created: function() {
-    // this.chapterId = 1;
-  },
-  data() {
-    return {
-      chapterId: 1,
-      course: {
-        id: 8,
-        posterUrl: 'https://szimg.mukewang.com/5c62a4dc0812e84106000338-360-202.jpg',
-        name: '数据结构',
-        description: '啥都不知道',
-        score: 4,
-        chapters: [
-          {
-            index: 1,
-            name: '第一章'
-          },
-          {
-            index: 2,
-            name: '第二章'
-          },
-          {
-            index: 3,
-            name: '第三章'
-          },
-          {
-            index: 4,
-            name: '第四章'
-          },
-          {
-            index: 5,
-            name: '第五章'
-          }
-        ]
-      }
-    }
   },
   computed: {
     notes: function() {
@@ -130,6 +121,44 @@ export default {
       ]
     }
   },
+  created: function() {
+    // this.chapterId = 1;
+  },
+  data() {
+    return {
+      chapterId: 1,
+      course: {
+        id: 8,
+        posterUrl: 'https://szimg.mukewang.com/5c62a4dc0812e84106000338-360-202.jpg',
+        name: '数据结构',
+        teacher: '许卓尔',
+        description: '数据结构是计算机存储、组织数据的方式。数据结构是指相互之间存在一种或多种特定关系的数据元素的集合。通常情况下，精心选择的数据结构可以带来更高的运行或者存储效率。数据结构往往同高效的检索算法和索引技术有',
+        score: 4,
+        chapters: [
+          {
+            index: 1,
+            name: '第一章'
+          },
+          {
+            index: 2,
+            name: '第二章'
+          },
+          {
+            index: 3,
+            name: '第三章'
+          },
+          {
+            index: 4,
+            name: '第四章'
+          },
+          {
+            index: 5,
+            name: '第五章'
+          }
+        ]
+      }
+    }
+  },
   methods: {
     handleClick(tab, event) {
       const id = tab.paneName
@@ -140,6 +169,10 @@ export default {
 </script>
 
 <style scoped>
+  img {
+    height: 100%;
+    width: 100%;
+  }
   .el-tabs {
     margin: 0 10% 100px 10%;
   }
@@ -152,12 +185,34 @@ export default {
   .course-info {
     padding: 0;
     height: 100%;
+    margin: 5px 10%;
   }
   .note-container {
     margin-bottom: 5px;
   }
   .el-tabs {
     padding-left: 20px;
+  }
+  .image{
+    margin-top: 10px;
+    margin-bottom:10px;
+    margin-left: 10px;
+    height: 100%;
+    width: 100%;
+  }
+  .course-title{
+    font-family:'Gill Sans';
+    margin-left: 10px;
+    margin-bottom: 20px;
+  }
+  .course-des{
+    margin-left: 10px;
+    color: gray;
+    margin-right: 150px;
+  }
+  .test{
+    margin-left: 30px;
+    margin-bottom: 40px;
   }
 </style>
 
